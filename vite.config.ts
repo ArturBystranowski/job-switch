4133dcbf11e7478b50dbf43ab6d16464e7be30e5
Artur Bystranowski <artur.bystranowski@cledar.com>
2025-11-16 23:18:22 +0100

    chore: enable React Compiler

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      babel: {
+        // React Compiler – optymalizacje renderowania komponentów
+        plugins: ['babel-plugin-react-compiler']
+      }
+    })
+  ],
   server: {
     port: 3000,
     open: true
